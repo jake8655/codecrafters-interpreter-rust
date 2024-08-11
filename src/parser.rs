8b1dccc22350b14d3ff43cd8670d2887a255f78c
Jake <dominik8655@gmail.com>
2024-08-11 16:43:48 +0200

    fix(parser): dont add trailing decimal places for number literals

--- src/parser.rs
+++ src/parser.rs
@@ -6,16 +6,16 @@ pub fn parse(file_contents: &str) -> Vec<String> {
 
     for token in tokens {
         match token {
-            scanner::Token::True
-            | scanner::Token::False
-            | scanner::Token::Nil
-            | scanner::Token::Number(_) => {
+            scanner::Token::True | scanner::Token::False | scanner::Token::Nil => {
                 let token_str = token.to_string();
                 let lower_idx = token_str.find(' ').unwrap();
                 let higher_idx = token_str.rfind(' ').unwrap();
 
                 let value = token_str[lower_idx + 1..higher_idx].to_string();
                 ast.push(value);
+            }
+            scanner::Token::Number(n) => {
+                ast.push(n.to_string());
             }
             scanner::Token::Eof => {}
             _ => {
